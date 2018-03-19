2563fdb48d6d70b0ae4549d65d8d2c5a5b7605cb
Kayvee <kv@github.com>
2018-03-19 12:43:38 -0600

    research data

---
+++ previousResearchs.cpp
@@ -0,0 +1,7 @@
+Goverment of canda - Privacy vs consent 
+    -https://www.priv.gc.                       ca/en/opc-actions-and-decisions/research/explore-privacy-research/2016/consent_201605/
+    - https://www.priv.gc.ca/en/opc-actions-and-decisions/research/explore-privacy-research/2016/iot_201602/
+    - https://www.loc.gov/law/help/online-privacy-law/canada.php
+
+Research paper 
+    - http://www.projectpact.eu/privacy-security-research-paper-series/privacy-security-research-paper-series/PACT_ResearchPapers_10_FINAL.pdf
